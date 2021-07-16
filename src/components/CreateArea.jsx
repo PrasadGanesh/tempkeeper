1137c4be6aa636755f4459650369190a1f183a95
Ganesh Prasad <ganeshprasad227@gmail.com>
2021-07-16 09:34:55 +0530

    Testing the updation 1

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add'
+import { Zoom } from "@material-ui/core";
+import { Fab } from "@material-ui/core";
+
+function CreateArea(props) {
+  const [clicked, setClicked] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        
+        {(clicked)?<input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />:null}
+        <textarea
+          name="content"
+          onClick={()=>{setClicked(!clicked)}}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={(clicked)?"3":"1"}
+        />
+        <Zoom in={clicked}>
+
+        <Fab onClick={submitNote}>
+          <AddIcon />
+        </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
