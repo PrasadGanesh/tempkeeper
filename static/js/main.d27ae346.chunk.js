84b7d31abbcb0db7459ca1f576535c7b5fa59279
Ganesh Prasad <ganeshprasad227@gmail.com>
2021-07-16 09:34:09 +0530

    Updates

--- static/js/main.d27ae346.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptempkeeper=this.webpackJsonptempkeeper||[]).push([[0],{9:function(e,t,r){"use strict";r.r(t);r(1);var n=r(3),c=r.n(n),s=r(0);var p=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hello World"})})};c.a.render(Object(s.jsx)(p,{}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.d27ae346.chunk.js.map
